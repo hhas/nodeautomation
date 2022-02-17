05698bb4ef40055d2671f37401490bc9dfe2802c
has <dev@nuggle.uk>
2022-02-17 20:37:52 +0000

    updated to latest `objc` revision

--- test/na-objc-test.js
+++ test/na-objc-test.js
@@ -28,18 +28,18 @@ ref = te.make({new: na.k.document, withProperties: {text: "Hello, World!"}})
 //console.log('ref: (type='+typeof ref+')') // ref: (type=function)
 
 
-console.log(ref) // TO DO: FIX: Specifiers aren't yet displaying correctly, i.e. "[Function (anonymous)]" instead of "app('TextEdit').documents.at(1)", but they are usable
+console.log(ref) // e.g. app('TextEdit').documents.named('Untitled 1')
 
 
 //console.log()
 
-console.log('get text:')
 let text = ref.text.get()
-console.log(text)
+console.log('get text:', text) // Hello, World!
 
 
 try {
 	te.documents[99].get()
 } catch(e) {
-	console.log(e)
+	console.log(e, '\n') // CommandError (-1719): Can't get reference. Invalid index.
 }
+
