a52f08654ac03f84e81e8bb3913fefcef473bf33
has <dev@nuggle.uk>
2022-02-22 10:54:06 +0000

    various bug fixes

--- test/test2.js
+++ test/test2.js
@@ -2,21 +2,19 @@
 
 'use strict';
 
-const na = require('./lib');
-const aesupport = na.__aesupport;
-
-const app = na.app, con = na.con, its = na.its, k = na.k, File = na.File;
-
-// TO DO: autorelease pools not yet implemented in NodeAutomation, so will leak unless user provides their own
+const {app, con, its, k, File} = require('nodeautomation');
 
 
 
-console.log(k, k.document, k.fromTypeCode("#docu")[aesupport.__packSelf](), k.fromTypeCode(0x75747874))
+try {
+
+console.log(app('TextEdit', {terminology: '/Users/has/dev/javascript/nodeautomation/test/TextEdit-glue.json'}).documents.first())
 
 
-var v = k.documentFile;
-console.log(k.documentFile.isEqual(v),k.documentfile.isEqual(v));
+} catch (e) {
 
+console.log(e)
 
-var file = new File('/Users/jsmith/Documents');
-console.log(file, String(file), new File('/Users/jsmith/Documents').isEqual(file), new File('/').isEqual(file));
+console.log(e.stack)
+
+}
